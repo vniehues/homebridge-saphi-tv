b78306d37208ddb11e520bdd7245b8d728c6b3e3
vniehues <vniehues@icloud.com>
2021-01-21 16:54:39 +0100

    some work ^^

--- src/platform.ts
+++ src/platform.ts
@@ -1,14 +1,14 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { TelevisionAccessory } from './platformAccessory';
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class SaphiTvPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
@@ -20,12 +20,8 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    this.log.info('Finished initializing platform:', this.config.name);
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
       // run the method to discover / register your devices as accessories
@@ -33,10 +29,6 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
 
@@ -54,24 +46,20 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
+    const Devices = [
       {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
+        uniqueId: 'SaphiTvDevice',
+        displayName: 'SaphiTv',
       },
     ];
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of Devices) {
 
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.uniqueId);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -81,40 +69,30 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // the accessory already exists
         if (device) {
           this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-          // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-          // existingAccessory.context.device = device;
-          // this.api.updatePlatformAccessories([existingAccessory]);
-
-          // create the accessory handler for the restored accessory
-          // this is imported from `platformAccessory.ts`
-          new ExamplePlatformAccessory(this, existingAccessory);
+          new TelevisionAccessory(this, existingAccessory, this.config);
           
           // update accessory cache with any changes to the accessory details and information
           this.api.updatePlatformAccessories([existingAccessory]);
         } else if (!device) {
-          // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
           // remove platform accessories when no longer present
           this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
           this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
         }
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
+        this.log.info('Adding new accessory:', device.displayName);
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
+        const accessory = new this.api.platformAccessory(device.displayName, uuid);
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
         accessory.context.device = device;
-
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
+        new TelevisionAccessory(this, accessory, this.config);
         // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        // this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+
+        this.api.publishExternalAccessories(PLUGIN_NAME, [accessory]);
       }
     }
   }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,151 +1,327 @@
-import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+/* eslint-disable @typescript-eslint/no-this-alias */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable max-len */
+import { Service, PlatformAccessory, CharacteristicSetCallback, CharacteristicGetCallback, PlatformConfig, Categories, Characteristic, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { SaphiTvPlatform } from './platform';
+
+import fetch from 'node-fetch';
+import wol from 'wake_on_lan';
+import { DH_UNABLE_TO_CHECK_GENERATOR } from 'constants';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class TelevisionAccessory {
+  input_url: string;  
+  
+  waitFor(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  wolRequest(url) {
+    return new Promise(resolve => setTimeout(() => {
+      const that = this;
+
+      this.platform.log.debug('calling WOL with URL %s', url);
+      if (!url) {
+        that.platform.log.warn('WOL-Error: ');
+        return;
+      }
+      if (url.substring(0, 3).toUpperCase() === 'WOL') {
+      //Wake on lan request
+        const macAddress = url.replace(/^WOL[:]?[/]?[/]?/gi, '');
+        this.platform.log.debug('Executing WakeOnLan request to ' + macAddress);
+        wol.wake(macAddress, (error) => {
+          if (error) {
+            that.platform.log.warn('WOL-Error: ', error);
+          } else {
+            that.platform.log.warn('WOL-OK!');
+          }
+        });
+      } else {
+        if (url.length > 3) {
+          that.platform.log.warn('WOL-Error: ');
+        } else {
+          that.platform.log.warn('WOL-Error: ');
+        }
+      }
+    }, 20));
+  }
+
   private service: Service;
 
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private TvState = {
+    TvActive: false,
+    AmbiHueActive: false,
   };
 
+  power_url: string;
+
+  protocol= 'http';
+
+  ip_address: string;
+
+  portNo= 1925;
+
+  api_version= 6;
+    
+  power_on_body = { powerstate: 'On' };
+
+  power_off_body = { powerstate: 'Standby' };
+  wol_url: string;
+
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: SaphiTvPlatform,
     private readonly accessory: PlatformAccessory,
+    public readonly config: PlatformConfig,
   ) {
+    this.ip_address = config.ip_adress as string;
+    this.wol_url = config.wol_adress as string;
+
+    this.input_url =
+    this.protocol +
+    'http://' +
+    this.ip_address +
+    ':' +
+    this.portNo +
+    '/' +
+    this.api_version +
+    '/input/key';
+    
+    // this.ambihue_url =
+    // this.protocol +
+    // "://" +
+    // this.ip_address +
+    // ":" +
+    // this.portNo +
+    // "/" +
+    // this.api_version +
+    // "/HueLamp/power";
+    
+    // this.ambihue_on_body = JSON.stringify({ power: "On" });
+    // this.ambihue_off_body = JSON.stringify({ power: "Off" });
+    
+    this.power_url =
+          this.protocol +
+          '://' +
+          this.ip_address +
+          ':' +
+          this.portNo +
+          '/' +
+          this.api_version +
+          '/powerstate';
+
+
+
+
+
+    // get/set the service 
+    this.service = this.accessory.getService(this.platform.Service.Television) || this.accessory.addService(this.platform.Service.Television);
 
     // set accessory information
+    this.accessory.category = Categories.TELEVISION;
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Test-Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    // set the tv name
+    this.service.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'test');
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    // set sleep discovery characteristic
+    this.service.setCharacteristic(this.platform.Characteristic.SleepDiscoveryMode, this.platform.Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    // handle on / off events using the Active characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .on('set', (newValue, callback) => {
+        this.SetActive(newValue, callback);
+        this.platform.log.info('set Active => setNewValue: ' + newValue);
+        // this.service.updateCharacteristic(this.platform.Characteristic.Active, 1);
+        // callback(null);
+      })
+      .on('get', (callback) => {
+        this.GetActive( callback);
+        this.platform.log.info('Get Active');
+        // this.service.updateCharacteristic(this.platform.Characteristic.Active, 1);
+        // callback(null);
+      }) ;
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service.setCharacteristic(this.platform.Characteristic.ActiveIdentifier, 1);
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    // handle input source changes
+    this.service.getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
+      .on('set', (newValue, callback) => {
 
+        // the value will be the value you set for the Identifier Characteristic
+        // on the Input Source service that was selected - see input sources below.
 
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+        this.platform.log.info('set Active Identifier => setNewValue: ' + newValue);
+        callback(null);
+      });
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    // handle remote control input
+    this.service.getCharacteristic(this.platform.Characteristic.RemoteKey)
+      .on('set', (newValue, callback) => {
+        switch(newValue) {
+          case this.platform.Characteristic.RemoteKey.REWIND: {
+            this.platform.log.info('set Remote Key Pressed: REWIND');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.FAST_FORWARD: {
+            this.platform.log.info('set Remote Key Pressed: FAST_FORWARD');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.NEXT_TRACK: {
+            this.platform.log.info('set Remote Key Pressed: NEXT_TRACK');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.PREVIOUS_TRACK: {
+            this.platform.log.info('set Remote Key Pressed: PREVIOUS_TRACK');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.ARROW_UP: {
+            this.platform.log.info('set Remote Key Pressed: ARROW_UP');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.ARROW_DOWN: {
+            this.platform.log.info('set Remote Key Pressed: ARROW_DOWN');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.ARROW_LEFT: {
+            this.platform.log.info('set Remote Key Pressed: ARROW_LEFT');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.ARROW_RIGHT: {
+            this.platform.log.info('set Remote Key Pressed: ARROW_RIGHT');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.SELECT: {
+            this.platform.log.info('set Remote Key Pressed: SELECT');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.BACK: {
+            this.platform.log.info('set Remote Key Pressed: BACK');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.EXIT: {
+            this.platform.log.info('set Remote Key Pressed: EXIT');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.PLAY_PAUSE: {
+            this.platform.log.info('set Remote Key Pressed: PLAY_PAUSE');
+            break;
+          }
+          case this.platform.Characteristic.RemoteKey.INFORMATION: {
+            this.platform.log.info('set Remote Key Pressed: INFORMATION');
+            break;
+          }
+        }
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+        // don't forget to callback!
+        callback(null);
+      });
 
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
-    let motionDetected = false;
     setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+
+      // TODO: Add getPowerState and getAmbilightState here and update every minute.
 
       // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+      // motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
+      // motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+      this.platform.log.debug('Triggering interval:', 'test');
+    }, 60000);
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  // setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+  //   // implement your own code to turn your device on/off
+  //   this.exampleStates.On = value as boolean;
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+  //   this.platform.log.debug('Set Characteristic On ->', value);
 
-    // you must call the callback function
-    callback(null);
-  }
+  // }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  getOn(callback: CharacteristicGetCallback) {
-
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
+  async GetActive(callback: CharacteristicGetCallback) {
+    
+    const isOn = this.TvState.TvActive;
     callback(null, isOn);
+
+    await fetch(this.power_url)
+      .then(response => response.json())
+      .then(result => {
+        this.platform.log.debug('Success:', result);
+        if(JSON.stringify(result) === JSON.stringify(this.power_on_body)) {
+          this.TvState.TvActive = true;
+        } else if(JSON.stringify(result) === JSON.stringify(this.power_off_body)) {
+          this.TvState.TvActive = false;
+        }
+      })
+      .catch(error => {
+        this.platform.log.debug('Error getPowerState : ', error);
+        this.TvState.TvActive = false;
+      });
+    this.service.updateCharacteristic(this.platform.Characteristic.Active, this.TvState.TvActive);
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-    // you must call the callback function
+  async SetActive(value: CharacteristicValue, callback: CharacteristicSetCallback) {
     callback(null);
-  }
+    const newPowerState = value === this.platform.Characteristic.Active.ACTIVE;
+    this.platform.log.debug('Setting power to: ', newPowerState);
+    if (newPowerState){
+      await this.wolRequest(this.wol_url);
+      //TODO: Hier Ambilight stuff einfügen
+      // .then(
+      //   async () => {
+      //     await this.waitFor(2000)
+      //       .then( async () => {
+      //         await fetch(this.input_url, {
+      //           method: 'POST', // or 'PUT'
+      //           headers: {
+      //             'Content-Type': 'application/json',
+      //           },
+      //           body: JSON.stringify({key: 'Standby'}),
+      //         });
+      //       },
+      //       );
+      //   },
+      // );
+    } else {
+      await fetch(this.input_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({key: 'Standby'}),
+      });
+      //TODO: Hier Ambilight stuff einfügen// .then(
+      //   async () => {
+      //     await this.waitFor(2000)
+      //       .then( async () => {
+      //         await fetch(this.input_url, {
+      //           method: 'POST', // or 'PUT'
+      //           headers: {
+      //             'Content-Type': 'application/json',
+      //           },
+      //           body: JSON.stringify({key: 'Standby'}),
+      //         });
+      //       },
+      //       );
+      //   },
+      // );
+    }
 
+    // const response = await fetch(
+    //   'https://jsonplaceholder.typicode.com/todos',
+    // );
+    // await this.waitFor(3000);
+    // // you must call the callback function
+  }
 }
