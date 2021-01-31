61cbf26a702db5f3433a1a336da50b92975628ff
Vincent Niehues <vniehues@icloud.com>
2021-01-31 16:11:24 +0100

    switches are now in a separate accessory;

--- src/platform.ts
+++ src/platform.ts
@@ -1,4 +1,4 @@
-import { API, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic, IndependentPlatformPlugin } from 'homebridge';
+import { API, APIEvent, Logger, PlatformAccessory, PlatformAccessoryEvent, PlatformConfig, Service, Characteristic, DynamicPlatformPlugin } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { TelevisionAccessory } from './platformAccessory';
@@ -8,7 +8,7 @@ import { TelevisionAccessory } from './platformAccessory';
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class SaphiTvPlatform implements IndependentPlatformPlugin {
+export class SaphiTvPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
@@ -20,21 +20,42 @@ export class SaphiTvPlatform implements IndependentPlatformPlugin {
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.publishExampleExternalAccessory();
-    this.log.info('Finished initializing platform:', PLATFORM_NAME);
+    this.log.info('Finished initializing platform: ' + PLATFORM_NAME);
+
+    api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+      this.log.info('Finished launching platform:', PLATFORM_NAME);
+      this.publishExampleExternalAccessory();
+    });
   }
 
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
 
+    accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+      this.log.info("%s identified", accessory.displayName);
+    });
+
     this.accessories.push(accessory);
   }
 
   publishExampleExternalAccessory() {
+    this.removeAllAccessories();
+
     const tvName = this.config.name || 'Saphi TV';
-    const uuid = this.api.hap.uuid.generate(PLUGIN_NAME + tvName);
-    const accessory = new this.api.platformAccessory(tvName, uuid);
-    new TelevisionAccessory(this, accessory, this.config);
-    this.api.publishExternalAccessories(PLUGIN_NAME, [accessory]);
+    const uuidTv = this.api.hap.uuid.generate(PLUGIN_NAME + tvName);
+    const tvAccessory = new this.api.platformAccessory(tvName, uuidTv);
+
+    const uuidRemote = this.api.hap.uuid.generate(PLUGIN_NAME + tvName + 'Remote');
+    const remoteAccessory = new this.api.platformAccessory(tvName + 'Remote', uuidRemote);
+
+    new TelevisionAccessory(this, tvAccessory,remoteAccessory, this.config);
+    this.api.publishExternalAccessories(PLUGIN_NAME, [tvAccessory]);
+    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [remoteAccessory]);
+  }
+
+  removeAllAccessories(){
+    this.log.info('removing all accessories');
+    this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, this.accessories);
+    this.accessories.splice(0, this.accessories.length);
   }
 }
